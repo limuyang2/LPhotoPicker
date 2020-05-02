5dcde47206aa77813635a26ddcd2a273b5b7c513
limuyang <limuyang2@163.com>
2020-05-02 21:25:39 +0800

    - 默认glide加载增加圆角

--- photolibrary/src/main/java/top/limuyang2/photolibrary/engine/LGlideEngine.kt
+++ photolibrary/src/main/java/top/limuyang2/photolibrary/engine/LGlideEngine.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
+import top.limuyang2.photolibrary.util.dip
 
 class LGlideEngine : LImageEngine {
 
@@ -17,6 +20,7 @@ class LGlideEngine : LImageEngine {
         Glide.with(context)
                 .load(path)
                 .apply(glideOptions.placeholder(placeholderRes).override(resizeX, resizeY))
+                .transform(CenterCrop(), RoundedCorners(context.dip(4).toInt()))
 //                .transition(glideTransition)
                 .into(imageView)
     }
